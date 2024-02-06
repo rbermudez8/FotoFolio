e9177ff31ed3ddc472c0a6363e5ac6b208cca548
rbermudez8 <rbermudez8@icloud.com>
2024-02-05 17:47:13 -0700

    Generated the Header, Gallery, and About Page components and modified app.module.ts for routing purposes.

--- FotoFolio/src/app/app-routing.module.ts
+++ FotoFolio/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GalleryComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FotoFolio/src/app/app.module.ts
+++ FotoFolio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
