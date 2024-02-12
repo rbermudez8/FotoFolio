91f078389729d2137f49d25ede9628bcc2cf4bf3
rbermudez8 <rbermudez8@icloud.com>
2024-02-12 00:46:05 -0700

    Added temporary logo picture and changes to gallery for url links to

--- FotoFolio/src/app/gallery/gallery.component.ts
+++ FotoFolio/src/app/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class GalleryComponent {
 
   images = [
-    { url: 'Laguna_Ventanilla.jpg', title: 'Playa', description: 'The sandbar between the ocean and Laguna Ventanilla in Oaxaca, Mexico' },
-    { url: 'Playa_de_Ziplote.jpg', title: 'Zipolite', description: 'Taken at the beach at Zipolite, Oaxaca, Mexico' },
-    { url: 'Punta_Cometa.jpg', title: 'Punta Cometa', description: 'Taken at Punta Cometa in Mazunte, Oaxaca, Mexico' },
+    { url: 'FotoFolio%2Fsrc%2Fassets%2FPhotos%2FLaguna_Ventanilla.jpg', title: 'Playa', description: 'The sandbar between the ocean and Laguna Ventanilla in Oaxaca, Mexico' },
+    { url: 'FotoFolio/src/assets/Photos/Playa_de_Ziplote.jpg', title: 'Zipolite', description: 'Taken at the beach at Zipolite, Oaxaca, Mexico' },
+    { url: 'FotoFolio/src/assets/Photos/Punta_Cometa.jpg', title: 'Punta Cometa', description: 'Taken at Punta Cometa in Mazunte, Oaxaca, Mexico' },
   ];
-
+  
   selectedImage:any;
 
   onImageClick(image: any) {
