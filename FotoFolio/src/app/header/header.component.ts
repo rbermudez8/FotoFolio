0e8f082717fc82509f55179c791e4e44f31fd169
rbermudez8 <rbermudez8@icloud.com>
2024-02-12 00:48:57 -0700

    Added temp icon for testing purposes only
    Adding temporary logo and modified type of icon image used in header.componet.

--- FotoFolio/src/app/header/header.component.ts
+++ FotoFolio/src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  logoUrl: string = "assets/logo.png"
+  logoUrl: string = "assets/logo.jpg"
 
   constructor() { }
 
